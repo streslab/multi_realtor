1e5c2fd97f5495c18c7c80b07229a53daca60649
Reed Slaby <reedslaby@gmail.com>
2017-07-23 22:17:00 -0500

    Building scraper

--- housescraper/housescraper/spiders/realtor.py
+++ housescraper/housescraper/spiders/realtor.py
@@ -6,8 +6,15 @@ from ..items import HousescraperItem
 class RealtorSpider(scrapy.Spider):
     name = 'realtor'
     allowed_domains = ['realtor.com']
-    start_urls = ['http://realtor.com/']
+    start_urls = ['http://realtor.com/realestateandhomes-search/Madison-WI/type-multi-family-home']
 
     def parse(self, response):
-        # TODO
-        return HousescraperItem(name='multifamily')
+        city = response.xpath()
+        state = response.xpath()
+        address = response.xpath()
+        beds = response.xpath()
+        baths = response.xpath()
+        sqft = response.xpath()
+        price = response.xpath()
+        url = response.xpath()
+        return HousescraperItem(city = city, state = state, address = address, beds = beds, baths = baths, sqft = sqft, price = price, url = url)
